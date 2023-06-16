acb7f9e778bcb1ecf16c7c4d921f4d763c61845a
stormymayday <stormymayday7@gmail.com>
2023-06-16 11:34:13 -0700

    Imprting getElement in app.js

--- app.js
+++ app.js
@@ -1,21 +1,3 @@
+import getElement from "./utils/getElement.js";
+
 const url = 'https://randomuser.me/api/';
-
-// const fetchRandomUser = async () => {
-
-//     try {
-
-//         const response = await fetch(url);
-
-//         const data = await response.json();
-
-//         return data;
-
-//     } catch (error) {
-
-//         console.error(error);
-
-//     }
-
-// };
-
-// console.log(fetchRandomUser());
