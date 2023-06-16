b0cc4352506361f8d550215390da75261b5211cf
stormymayday <stormymayday7@gmail.com>
2023-06-16 11:48:48 -0700

    Selecting the '.user-title' paragraph

--- app.js
+++ app.js
@@ -4,5 +4,6 @@ const url = 'https://randomuser.me/api/';
 
 // Selections
 const img = getElement('.user-img');
+const title = getElement('.user-title');
 const btn = getElement('.btn');
-console.log(btn);
+console.log(title);
