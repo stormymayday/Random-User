7bf529a9cf7be4b3415edf6b4beb3e5bac02d7fc
stormymayday <stormymayday7@gmail.com>
2023-06-16 11:59:55 -0700

    Selecting all '.icon' buttons and transforing the Nodelist into an array using the spread operator

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ import getElement from "./utils/getElement.js";
 
 const url = 'https://randomuser.me/api/';
 
-// Selections
+// Element Selections
 const img = getElement('.user-img');
 const title = getElement('.user-title');
 const value = getElement('.user-value');
 const btn = getElement('.btn');
-console.log(value);
+const btns = [...document.querySelectorAll('.icon')];
+console.log(btns);
