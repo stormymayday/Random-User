abdd8fddd11887b14f2b35329140d762e212db32
stormymayday <stormymayday7@gmail.com>
2023-06-16 11:29:31 -0700

    Tesing the endpoint via fetchRandomUser function

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 const url = 'https://randomuser.me/api/';
+
+// const fetchRandomUser = async () => {
+
+//     try {
+
+//         const response = await fetch(url);
+
+//         const data = await response.json();
+
+//         return data;
+
+//     } catch (error) {
+
+//         console.error(error);
+
+//     }
+
+// };
+
+// console.log(fetchRandomUser());
