cad44caa81a9e7cd166c80f2dbfdd75d48a10aca
stormymayday <stormymayday7@gmail.com>
2023-06-27 19:25:00 -0700

    Removed the img selection

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import getElement from "./utils/getElement.js";
 import getUser from "./utils/fetchUser.js";
 import displayUser from "./utils/displayUser.js";
 
-const img = getElement('.user-img');
 const title = getElement('.user-title');
 const value = getElement('.user-value');
 const btn = getElement('.btn');
