a0222d334e6d450474c06b8933da0e93f6117b08
stormymayday <stormymayday7@gmail.com>
2023-06-27 19:25:50 -0700

    Removed the value selection

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import getElement from "./utils/getElement.js";
 import getUser from "./utils/fetchUser.js";
 import displayUser from "./utils/displayUser.js";
 
-const value = getElement('.user-value');
 const btn = getElement('.btn');
 const btns = [...document.querySelectorAll('.icon')];
 
